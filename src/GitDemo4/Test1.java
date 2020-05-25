1cdc4aee0b1e1f6ea69ce6d973ddea1592879d6e
paulD-byte <65722799+paulD-byte@users.noreply.github.com>
2020-05-25 12:49:17 +0530

    Fourth Commit....

---
+++ src/GitDemo4/Test1.java
@@ -0,0 +1,16 @@
+package GitDemo4;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("Eat Someyhing......");
+		} else {
+			System.out.println("Do Work...........");
+		}
+	}
+}
